9e3c1a5eee5df7801196841b01da56baeb647bd6
joseph debbah <joseph.debbah.local@gmail.com>
2021-05-23 21:39:37 +0100

    fix identifier regex

--- src/main/java/dz/ngnex/entity/Constrains.java
+++ src/main/java/dz/ngnex/entity/Constrains.java
@@ -19,7 +19,7 @@ public final class Constrains {
   private static final String FRENCH_ACCENTS = "\u00e7\u00e9\u00e2\u00ea\u00ee\u00f4\u00fb\u00e0\u00e8\u00f9\u00eb\u00ef\u00fc\u00c7\u00c9\u00c2\u00ca\u00ce\u00d4\u00db\u00c0\u00c8\u00d9\u00cb\u00cf\u00dc";
   private static final String ALPHANUMERIC_CHAR = "(?:\\w|\\d|[" + FRENCH_ACCENTS + "])";
   public static final Pattern IDENTIFIER_PATTERN = Pattern.compile(ALPHANUMERIC_CHAR +
-      "(?:\\w|\\d|[ _-" + FRENCH_ACCENTS + "])*" + ALPHANUMERIC_CHAR);
+      "(?:\\w|\\d|[- _" + FRENCH_ACCENTS + "])*" + ALPHANUMERIC_CHAR);
 
   @Language("RegExp")
   public static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
